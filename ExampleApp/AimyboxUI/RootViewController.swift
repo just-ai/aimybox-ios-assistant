e314ffe43bb0bb46c5e089270c42c0f67f29819f
Vladyslav Popovych <vladislav.popovich@ignited.it>
2019-12-26 20:10:22 +0200

    example app show

---
+++ ExampleApp/AimyboxUI/RootViewController.swift
@@ -0,0 +1,50 @@
+//
+//  RootViewController.swift
+//  AimyboxUI
+//
+//  Created by Vladislav Popovich on 26.12.2019.
+//  Copyright © 2019 NSI. All rights reserved.
+//
+
+import Aimybox
+import AimyboxUI
+
+class RootViewController: UIViewController, AimyboxProvider {
+
+    func aimybox() -> Aimybox? {
+        let locale = Locale(identifier: "ru")
+        
+        guard let speechToText = SFSpeechToText(locale: locale) else {
+            fatalError("Locale is not supported.")
+        }
+        guard let textToSpeech = AVTextToSpeech(locale: locale) else {
+            fatalError("Locale is not supported.")
+        }
+        
+        let dialogAPI = AimyboxDialogAPI(api_key: "sgEfvEonbLOTw6wTEaINZb6zehab8RQF",
+                                         unit_key: UIDevice.current.identifierForVendor!.uuidString,
+                                         route: URL(string: "https://bot.aimylogic.com/chatapi/webhook/zenbox/cVcGlsvz:800911b5cd537cba8c734e772f8c4a1ebd68fb1a")!)
+        
+        let config = AimyboxBuilder.config(speechToText, textToSpeech, dialogAPI)
+        
+        return AimyboxBuilder.aimybox(with: config)
+    }
+
+    @IBOutlet weak var aimyboxOpenButton: AimyboxOpenButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        aimyboxOpenButton.presenter = self
+        
+        AimyboxViewController.onViewDidLoad = { vc in
+
+            let aimyboxView = vc.aimyboxView
+            
+            aimyboxView.provider = self
+            aimyboxView.backgroundColor = .systemYellow
+            aimyboxView.greetingText = "Привет, чем я могу помочь?"
+            aimyboxView.emptyRecognitionText = "Попробуй повторить..."
+        }
+    }
+}
---
+++ ExampleApp/Podfile
@@ -0,0 +1,12 @@
+platform :ios, '11.4'
+
+target 'AimyboxUI' do
+  use_frameworks!
+
+  pod 'AimyboxUI'
+  pod 'Aimybox/Core'
+  pod 'Aimybox/AimyboxDialogAPI'
+  pod 'Aimybox/SFSpeechToText'
+  pod 'Aimybox/AVTextToSpeech'
+
+end
