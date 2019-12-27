876314f5a2bac196d5186967611661bc893cf896
Vladyslav Popovych <mailuatc@gmail.com>
2019-12-27 02:46:20 +0200

    Update AimyboxUILib.h

--- AimyboxUILib/AimyboxUILib.h
+++ AimyboxUILib/AimyboxUILib.h
@@ -3,7 +3,7 @@
 //  AimyboxUILib
 //
 //  Created by Vladislav Popovich on 23.12.2019.
-//  Copyright © 2019 NSI. All rights reserved.
+//  Copyright © 2019 Just Ai. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
